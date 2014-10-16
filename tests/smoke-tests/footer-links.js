ee10071710e63de5294014227cf3b1977a3363c6
Boris Yankov <borisyankov@gmail.com>
2014-10-16 11:41:18 +0800

    Fix smoke tests

--- tests/smoke-tests/footer-links.js
+++ tests/smoke-tests/footer-links.js
@@ -8,9 +8,9 @@ var urls = [
     { page: 'Responsible Trading', path: URL.RESPONSIBLE_TRADING },
     { page: 'Terms and Conditions', path: URL.TERMS_AND_CONDITIONS },
 
-    { page: 'Pricing Table', path: URL.PRICING_TABLE },
-    { page: 'Rise/Fall Table', path: URL.RISE_FALL_TABLE },
-    { page: 'Asset Index', path: URL.ASSET_INDEX },
+    { page: 'Pricing Table', path: URL.RESOURCES.PRICING_TABLE },
+    { page: 'Rise/Fall Table', path: URL.RESOURCES.RISE_FALL_TABLE },
+    { page: 'Asset Index', path: URL.RESOURCES.ASSET_INDEX },
 
     { page: 'Payment Methods', path: URL.PAYMENT_METHODS },
     { page: 'Security and Privacy', path: URL.SECURITY_AND_PRIVACY },
@@ -19,9 +19,9 @@ var urls = [
     { page: 'Getting Started', path: URL.GETTING_STARTED },
     { page: 'Platform Tour', path: URL.PLATFORM_TOUR },
 
-    { page: 'Chart Application', path: URL.CHART_APPLICATION },
-    { page: 'Light Charts', path: URL.LIGHT_CHARTS },
-    { page: 'Live Charts', path: URL.LIVE_CHARTS },
+    { page: 'Chart Application', path: URL.CHARTS.JAVA_CHARTS },
+    { page: 'Light Charts', path: URL.CHARTS.LIGHT_CHARTS },
+    { page: 'Live Charts', path: URL.CHARTS.LIVE_CHARTS },
 
     { page: 'Group Information', path: URL.GROUP_INFORMATION },
     { page: 'Group History', path: URL.GROUP_HISTORY },
