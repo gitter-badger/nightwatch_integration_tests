2fb9c7d83449633f26cae809119db66abe213a20
Boris Yankov <borisyankov@gmail.com>
2015-01-28 16:27:52 +0200

    Implement custom reporter that lists all (but only) failed tests after the full run

--- globals.js
+++ globals.js
@@ -17,7 +17,21 @@ module.exports = {
         randomStr: randomStr
     },
     reporter : function(results, done) {
-        console.log(results);
+
+        if (results.failed === 0) return;
+
+        console.log('\nList of failed tests:');
+
+        Object.keys(results.modules).forEach(function(x) {
+            var module = results.modules[x];
+            Object.keys(module).forEach(function(suite) {
+                module[suite].tests.forEach(function(test) {
+                    if (test.failure) {
+                        console.log('\n', suite, '\n', test.message, '\n', test.failure);
+                    }
+                });
+            });
+        });
         done();
     }
 };
