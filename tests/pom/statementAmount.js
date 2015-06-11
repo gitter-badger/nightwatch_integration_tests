a6f6dfba0aaee3cf8e60f5c7075eb45c879fa21c
Negar Naghshbandi <negar@binary.com>
2015-06-11 16:07:45 +0800

    New client's statement shows only a credit of 10,000 USD

---
+++ tests/pom/statementAmount.js
@@ -0,0 +1,29 @@
+//New client's statement shows only a credit of $10,000
+
+var createVirtualAccount= require('./createVirtualAccount').createVirtualAccount;
+var viewStatement= require ('./viewStatement').viewStatement;
+
+module.exports = {
+		
+		before: function(browser) {
+				console.log('Setting up...');
+				createVirtualAccount(browser, true);
+				viewStatement(browser, true);
+		},
+			
+			
+		after: function(browser) {
+				console.log('Closing down...');
+		},
+		
+				 		 
+		'statementAmount': function (browser) {
+									
+			browser
+		   		.page.account().verifyStatementAmount()
+				.end();
+		   
+	  }
+		
+
+};
